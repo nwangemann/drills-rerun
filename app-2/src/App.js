663379abe3dd4b532644851ec41b099940b64f7a
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-04-28 20:17:57 -0700

    finishes app2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const defaultList = ['one', 'two', 'three']
+
 function App() {
+  const [list, setList] = useState(defaultList)
+  const mappedList = list.map(elem => {
+  return <li>{elem}</li>
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ul>
+        {mappedList}
+      </ul>
+    </React.Fragment>
   );
 }
 
