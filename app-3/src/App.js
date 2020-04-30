153303bcac69fcf80d45630222e1fdda43600303
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-04-29 17:44:24 -0700

    finishes app 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -4,11 +4,10 @@ import "./App.css";
 function App() {
   const [list, setList] = useState(['first', 'second', 'third', 'fourth', 'fifth']);
   const [input, setInput] = useState("");
-  
 
-    let filteredList = list.filter((elem) => {
-      return (elem.startsWith(input))
-    });
+  let filteredList = list.filter((elem) => {
+    return (elem.startsWith(input))
+  });
     
   let mappedList = filteredList.map((elem) => {
     return <li>{elem}</li>;
@@ -16,12 +15,12 @@ function App() {
   return (
     <React.Fragment>
       <ul>
-        {mappedList}
         <input
           type="text"
           onChange={(e) => setInput(e.target.value)}
           name="input"
         />
+        {mappedList}
       </ul>
     </React.Fragment>
   );
