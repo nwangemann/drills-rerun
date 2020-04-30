2b865db092ba5013ea03679be97d39b8a7d54698
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-04-29 17:43:53 -0700

    finishes app 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState(['first', 'second', 'third', 'fourth', 'fifth']);
+  const [input, setInput] = useState("");
+  
+
+    let filteredList = list.filter((elem) => {
+      return (elem.startsWith(input))
+    });
+    
+  let mappedList = filteredList.map((elem) => {
+    return <li>{elem}</li>;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ul>
+        {mappedList}
+        <input
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+          name="input"
+        />
+      </ul>
+    </React.Fragment>
   );
 }
 
